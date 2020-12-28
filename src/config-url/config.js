80351088ee3e4e8cb6dfc23ce25617fbe6d425b0
wangwenlin <wangwenlin@innotechx.com>
2020-12-28 19:08:59 +0800

    对接api

--- src/config-url/config.js
+++ src/config-url/config.js
@@ -1,2 +1,2 @@
-// export const apiUrl = "http://sever.jddianqi.cn/admin";
-export const apiUrl = "http://8.129.76.170:9001/admin";
+export const apiUrl = "http://sever.jddianqi.cn/admin";
+// export const apiUrl = "http://8.129.76.170:9001/admin";
